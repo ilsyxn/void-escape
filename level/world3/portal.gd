feb5d08daab2ecdb47f67cbe413be3acefa416ba
Ileyan <3003916@stud.hs-mannheim.de>
2024-06-22 15:50:53 +0200

    CPU Partikel benutzt
    sonst stürzt es ab

--- level/world3/portal.gd
+++ level/world3/portal.gd
@@ -2,10 +2,10 @@ extends Node2D
 @export var map_position : Vector2i
 @export var connected_portal: Vector2i
 @export var farbe : GradientTexture1D
-@onready var particles = $GPUParticles2D
+@onready var particles = $CPUParticles2D
 
 func _ready():
-	particles.process_material.color_ramp.gradient = farbe.gradient
+	particles.color_ramp = farbe.gradient
 	
 
 
